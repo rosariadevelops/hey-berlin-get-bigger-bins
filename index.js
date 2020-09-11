eaf7f10dc42f419ef26431729f366f09c9476b32
Rosaria Gandar <rosariagandar@gmail.com>
2020-09-11 16:15:10 +0200

    I think cookie middleware is working

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-const requireUnsignedUser = (req, res, next) => {
-    if (!req.session.userId && req.url != '/login' && req.url != '/sign-up') {
+const requireLoggedOutUser = (req, res, next) => {
+    if (!req.session.userId && req.url != '/sign-in' && req.url != '/sign-up') {
         res.redirect('/sign-up');
     } else {
         next();
@@ -39,16 +39,24 @@ const requireUnsignedUser = (req, res, next) => {
 };
 
 const requireLoggedInUser = (req, res, next) => {
-    if (req.session.hasLoggedIn) {
+    if (req.session.userId) {
         res.redirect('/thanks');
     } else {
         next();
     }
 };
 
-const requireLoggedOutUser = (req, res, next) => {
-    if (!req.session.hasLoggedIn) {
-        res.redirect('/log-in');
+const requireHasSigned = (req, res, next) => {
+    if (req.session.hasSigned) {
+        res.redirect('/thanks');
+    } else {
+        next();
+    }
+};
+
+const requireHasNotSigned = (req, res, next) => {
+    if (!req.session.hasSigned) {
+        res.redirect('/petition');
     } else {
         next();
     }
@@ -95,8 +103,6 @@ app.post('/sign-up', (req, res) => {
                 console.log('req body password: ', pword);
                 // return example:  $2a$10$zpPuwhpqORBO0pbDTFgxSO0hAIKDsXbn0twuDAZCmNtAEI.iLA5RS
                 db.addUser(firstname, lastname, email, pword).then((result) => {
-                    req.session.userCreated = true;
-                    req.session.hasLoggedIn = true;
                     req.session.userId = result.rows[0].id;
                     console.log('user created');
                     res.redirect('/addprofile');
@@ -109,27 +115,21 @@ app.post('/sign-up', (req, res) => {
 });
 
 // PROFILE PAGE GET REQUEST
-app.get('/addprofile', requireUnsignedUser, requireLoggedInUser, (req, res) => {
-    if (!req.session.userCreated) {
-        res.redirect('/sign-up');
-    } else {
-        res.render('addprofile', {
-            layout: 'main',
-        });
-    }
+app.get('/addprofile', requireLoggedInUser, (req, res) => {
+    res.render('addprofile', {
+        layout: 'main',
+    });
 });
 
 // PROFILE PAGE POST REQUEST
 app.post('/addprofile', (req, res) => {
-    //const errMsg = document.getElementById('error');
     let { age, city, url, user_id } = req.body;
     user_id = req.session.userId;
     console.log('req.body: ', req.body);
 
     db.createProfile(age, city, url, user_id)
         .then((profile) => {
-            const profId = profile.rows[0].user_id;
-            req.session.profId = profId;
+            console.log('profile: ', profile);
             res.redirect('/petition');
         })
         .catch((err) => {
@@ -138,55 +138,45 @@ app.post('/addprofile', (req, res) => {
 });
 
 // LOGIN PAGE GET REQUEST
-app.get('/log-in', requireUnsignedUser, requireLoggedInUser, (req, res) => {
-    if (!req.session.userCreated) {
-        res.redirect('/sign-up');
-    } else {
-        res.render('login', {
-            layout: 'main',
-            title: 'Please log-in',
-        });
-    }
+app.get('/sign-in', requireLoggedInUser, (req, res) => {
+    res.render('login', {
+        layout: 'main',
+    });
 });
 
 // LOGIN PAGE POST REQUEST
-app.post('/log-in', (req, res) => {
+app.post('/sign-in', (req, res) => {
     const { firstname, lastname, email, password } = req.body;
-    //const errMsg = document.getElementById('error');
 
     if (firstname === '' || lastname === '' || email === '' || password === '') {
-        res.render('log-in', {
+        res.render('login', {
             layout: 'main',
-            title: 'Please log-in',
-            error: 'Please make sure all input fields have been filled.',
-            class: '"error"',
+            emptyerror: 'Please fill all fields to proceed',
         });
     } else {
         db.checkEmail(email)
             .then((results) => {
-                bc.compare(password, results.rows[0].pword)
-                    .then((exists) => {
-                        if (exists) {
-                            console.log('result is: ', exists);
+                console.log('results: ', results.rows);
+                if (results.rows == undefined) {
+                    console.log('result does not match any existing account');
+                    res.render('login', {
+                        layout: 'main',
+                        wrongCreds: 'The entered email or password are incorrect. Please try again',
+                    });
+                } else {
+                    bc.compare(password, results.rows[0].pword)
+                        .then((result) => {
+                            console.log('result:', result);
+                            console.log('user account found');
                             const userId = results.rows[0].id;
                             console.log('userId', userId);
-                            //req.session.userExists = true;
-                            req.session.hasLoggedIn = true;
                             req.session.userId = userId;
                             res.redirect('/thanks');
-                        } else {
-                            console.log('result is: ', exists);
-                            res.render('log-in', {
-                                layout: 'main',
-                                title: 'Please log-in',
-                                wrongEmail: 'Your email or password are incorrect.',
-                                class: '"error"',
-                            });
-                        }
-                    })
-                    .catch((err) => {
-                        console.log('err in compare: ', err);
-                    });
+                        })
+                        .catch((err) => {
+                            console.log('err in compare: ', err);
+                        });
+                }
             })
             .catch((err) => {
                 console.log('err in checkEmail: ', err);
@@ -195,39 +185,27 @@ app.post('/log-in', (req, res) => {
 });
 
 // PETITION PAGE GET REQUEST
-app.get('/petition', requireUnsignedUser, requireLoggedOutUser, (req, res) => {
-    if (req.session.hasSigned) {
-        res.redirect('/thanks');
-    } else {
-        res.render('petition', {
-            layout: 'main',
-            title: 'Petition',
-            error: 'Please sign your signature.',
-        });
-    }
+app.get('/petition', requireLoggedOutUser, requireHasSigned, (req, res) => {
+    res.render('petition', {
+        layout: 'main',
+    });
 });
 
 // PETITION POST REQUEST
 app.post('/petition', (req, res) => {
     let { sig, user_id } = req.body;
     user_id = req.session.userId;
-    //const errMsg = document.getElementById('error');
 
     if (sig === '') {
         res.render('petition', {
             layout: 'main',
-            title: 'Petition',
-            error: 'Please sign your signature.',
-            class: '"error"',
+            sigerror: 'Please sign your signature.',
         });
-        // I don't want to render this page again, I only want the error to appear
-        //errMsg.addClass('errVisible');
     } else {
         db.addSig(sig, user_id)
             .then((idNo) => {
+                console.log('idNo: ', idNo);
                 req.session.hasSigned = true;
-                req.session.sigIdNumber = idNo.rows[0].id;
-                req.session.sigPic = idNo.rows[0].signature;
                 res.redirect('/thanks');
             })
             .catch((err) => {
@@ -237,31 +215,26 @@ app.post('/petition', (req, res) => {
 });
 
 // THANKS GET REQUEST
-app.get('/thanks', requireUnsignedUser, requireLoggedOutUser, (req, res) => {
-    console.log('req.session.sigIdNumber: ', req.session.sigIdNumber);
-    if (!req.session.hasSigned) {
-        res.redirect('/petition');
-    } else {
-        db.getSignature(req.session.sigIdNumber).then((signee) => {
-            db.getSignedUsers()
-                .then((result) => {
-                    const numOfSigs = result.rows.length;
-                    console.log('numOfSigs: ', numOfSigs);
-                    const userSig = signee.rows[0].sig;
-                    // let firstName = userFirst.charAt(0).toUpperCase() + userFirst.slice(1);
-                    // let lastName = userLast.charAt(0).toUpperCase() + userLast.slice(1);
-                    res.render('thanks', {
-                        layout: 'main',
-                        title: 'Thank you!',
-                        userSig,
-                        numOfSigs,
-                    });
-                })
-                .catch((err) => {
-                    console.log('err in getSigner: ', err);
+app.get('/thanks', requireLoggedOutUser, requireHasNotSigned, (req, res) => {
+    db.getSignature(req.session.sigIdNumber).then((signee) => {
+        db.getSignedUsers()
+            .then((result) => {
+                const numOfSigs = result.rows.length;
+                console.log('numOfSigs: ', numOfSigs);
+                const userSig = signee.rows[0].sig;
+                // let firstName = userFirst.charAt(0).toUpperCase() + userFirst.slice(1);
+                // let lastName = userLast.charAt(0).toUpperCase() + userLast.slice(1);
+                res.render('thanks', {
+                    layout: 'main',
+                    title: 'Thank you!',
+                    userSig,
+                    numOfSigs,
                 });
-        });
-    }
+            })
+            .catch((err) => {
+                console.log('err in getSigner: ', err);
+            });
+    });
 });
 
 // THANKS PAGE TO DELETE SIGNATURE POST REQUEST
@@ -306,85 +279,72 @@ app.post('/thanks', (req, res) => {
 });
 
 // SIGNERS TEMPLATE GET REQUEST
-app.get('/signers', requireUnsignedUser, requireLoggedOutUser, (req, res) => {
-    if (!req.session.hasSigned) {
-        res.redirect('/petition');
-    } else {
-        db.popProfile()
-            .then((data) => {
-                const allSigners = data.rows;
-                const numOfSigs = allSigners.length;
-                console.log('allSigners: ', allSigners);
-                res.render('signers', {
-                    layout: 'main',
-                    title: 'Like-minded individuals',
-                    numOfSigs,
-                    allSigners,
-                });
-            })
-            .catch((err) => {
-                console.log('err in popProfile signers template: ', err);
-            });
-    }
-});
-
-// SIGNERS BY CITY TEMPLATE GET REQUEST
-app.get('/signers/:city', requireUnsignedUser, requireLoggedOutUser, (req, res) => {
-    let { city } = req.params;
-    console.log('city: ', city);
-    if (!req.session.hasSigned) {
-        res.redirect('/petition');
-    } else {
-        db.getCity(city).then((cityData) => {
-            console.log('city data: ', cityData.rows);
-            const numOfSigs = cityData.rows.length;
-            console.log('numOfSigs: ', numOfSigs);
-            const allSigners = cityData.rows;
+app.get('/signers', requireLoggedOutUser, requireHasNotSigned, (req, res) => {
+    db.popProfile()
+        .then((data) => {
+            const allSigners = data.rows;
+            const numOfSigs = allSigners.length;
+            console.log('allSigners: ', allSigners);
             res.render('signers', {
                 layout: 'main',
-                title: 'All fellow signers from: ',
-                city,
+                title: 'Like-minded individuals',
                 numOfSigs,
                 allSigners,
             });
+        })
+        .catch((err) => {
+            console.log('err in popProfile signers template: ', err);
         });
-    }
+});
+
+// SIGNERS BY CITY TEMPLATE GET REQUEST
+app.get('/signers/:city', requireLoggedOutUser, requireHasNotSigned, (req, res) => {
+    let { city } = req.params;
+    console.log('city: ', city);
+    db.getCity(city).then((cityData) => {
+        console.log('city data: ', cityData.rows);
+        const numOfSigs = cityData.rows.length;
+        console.log('numOfSigs: ', numOfSigs);
+        const allSigners = cityData.rows;
+        res.render('signers', {
+            layout: 'main',
+            title: 'All fellow signers from: ',
+            city,
+            numOfSigs,
+            allSigners,
+        });
+    });
 });
 
 // EDIT PROFILE PAGE GET REQUEST
-app.get('/profile/edit', requireUnsignedUser, requireLoggedOutUser, (req, res) => {
+app.get('/profile/edit', requireLoggedOutUser, (req, res) => {
     let userId = req.session.userId;
     console.log('user-id: ', userId);
 
-    if (!req.session.userCreated) {
-        res.redirect('/sign-up');
-    } else {
-        db.getUserInfo(userId)
-            .then((data) => {
-                const userDetails = data.rows[0];
-                console.log('userDetails: ', userDetails);
-                const { firstname, lastname, email, password, age, city, url } = userDetails;
-                res.render('edit', {
-                    layout: 'main',
-                    title: 'Edit your profile',
-                    firstname,
-                    lastname,
-                    email,
-                    password,
-                    age,
-                    city,
-                    url,
-                });
-            })
-            .catch((err) => {
-                console.log('err in getUserInfo: ', err);
+    db.getUserInfo(userId)
+        .then((data) => {
+            const userDetails = data.rows[0];
+            console.log('userDetails: ', userDetails);
+            const { firstname, lastname, email, password, age, city, url } = userDetails;
+            res.render('edit', {
+                layout: 'main',
+                title: 'Edit your profile',
+                firstname,
+                lastname,
+                email,
+                password,
+                age,
+                city,
+                url,
             });
-    }
+        })
+        .catch((err) => {
+            console.log('err in getUserInfo: ', err);
+        });
 });
 
 // EDIT PROFILE PAGE POST REQUEST
 app.post('/profile/edit', (req, res) => {
-    //const errMsg = document.getElementById('error');
     console.log('req body: ', req.body);
     let { firstname, lastname, email, password, age, city, url, user_id } = req.body;
     user_id = req.session.userId;
@@ -417,7 +377,6 @@ app.post('/profile/edit', (req, res) => {
         });
     } else {
         bc.hash(password).then((password) => {
-            //const password = pword;
             console.log('req body password: ', password);
 
             db.updatePassword(user_id, firstname, lastname, email, password).then(() => {
@@ -449,7 +408,7 @@ app.post('/profile/edit', (req, res) => {
 app.get('/signout', (req, res) => {
     req.session.userId = null;
     req.session.hasLoggedIn = null;
-    res.redirect('/login');
+    res.redirect('/log-in');
 });
 
 // LISTEN
